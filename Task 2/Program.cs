0ca8437bef8bdea19a4da978a0a76a616ab7ef27
Victoria Pavlova <Pavlova_V@avp.ru>
2023-09-25 20:32:40 +0300

    .\Task 2.csproj

---
+++ Task 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 >= number2)
+{
+    Console.Write($"максимальное число { number1}");
+}
+else
+{
+    Console.Write($"максимальное число { number2}");
+}
