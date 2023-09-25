c61f7063f1de871ffe6d650a5355ed3eb7996009
Victoria Pavlova <Pavlova_V@avp.ru>
2023-09-25 20:27:52 +0300

    .\Task1.csproj

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 == number2*number2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число НЕ является квадратом второго");
+}
